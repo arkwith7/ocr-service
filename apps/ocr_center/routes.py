2f526408aee6d9d0cb8d6fefef292e153f3a6d7e
Your Name <you@example.com>
2022-08-03 11:25:34 +0900

    20220803-오전

---
+++ apps/ocr_center/routes.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from flask import render_template, redirect, request, url_for
+from flask_login import (
+    current_user,
+    login_user,
+    logout_user
+)
+
+from apps import db, login_manager
+from apps.authentication import blueprint
+from apps.authentication.forms import LoginForm, CreateAccountForm
+from apps.authentication.models import Users
+
+from apps.authentication.util import verify_pass
+
+
